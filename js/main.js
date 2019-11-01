35ba6146092897d08ad705293eadbbc352949582
Devan Terry <devanterry@Devans-MacBook-Pro.local>
2019-11-01 09:55:37 -0700

    added html bp and pseudocode

---
+++ js/main.js
@@ -0,0 +1,35 @@
+// BlackJack Game Pseudocode
+
+// 1. The game will consist of two players - player and dealer. 
+
+// 2. There will be three buttons, one at the top of the page that begins the game, and two 
+// side by side at the bottom that will read 'Stay' and 'Hit Me'
+
+// 3. After pressing the 'Begin button, the player and dealer will both be randomly dealt
+// two cards of a 52-card deck, with four suites of 13 cards (2-10, Jack, Queen, King, Ace).
+
+// 4. The player must then decide whether to press the 'Hit Me' or 'Stay' buttons. Hit me will
+// solicite another random card being drawn from the deck.
+
+// 5. If the total value of the player's cards happens to total 21, the player wins.
+
+// 6. There must be game logic incorporated that will determine the 'choice' of the dealer 
+// whether to 'Stay' or 'Hit Me'. 
+
+// 7. If either the player or dealer chooses 'Hit Me' and the card value totals exceed 21, 
+// they will 'Bust' and automatically lose the game.
+
+// 8. In the event that both the dealer and player 'Stay', the winner will be the player 
+// with the highest card count, as long as it is not exceeding 21.
+
+
+
+//---------constants--------//
+
+//---------app's state (variables--------//
+
+//---------cached element references--------//
+
+//---------event listeners--------//
+
+//---------functions--------//
